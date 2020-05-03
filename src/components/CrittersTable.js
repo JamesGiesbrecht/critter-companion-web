4ffc07b36055e2b49d4cbf8ea3a7e530d6984ad8
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-05-03 13:19:28 -0500

    Basic critters table

--- src/components/CrittersTable.js
+++ src/components/CrittersTable.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { TableContainer, Table, TableBody, TableRow, TableCell, } from '@material-ui/core'
 import critters from '../assets/data/critters.json'
 
 const CrittersTable = () => {
   return (
-    <div>
-      CRITTERS
-    </div>
+    <TableContainer>
+      <Table
+        // className={classes.table}
+        aria-labelledby="tableTitle"
+        aria-label="enhanced table"
+      >
+        <TableBody>
+          {critters.map((critter, index) => (
+            <TableRow
+              hover
+              tabIndex={-1}
+              key={critter.name}
+            >
+              <TableCell component="th" id={index} scope="critter" padding="none">
+                {critter.name}
+              </TableCell>
+              <TableCell align="right">{critter.type}</TableCell>
+              <TableCell align="right">{critter.location}</TableCell>
+              <TableCell align="right">{critter.value}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
