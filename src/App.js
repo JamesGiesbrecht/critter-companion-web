059a765fb31821d7e18fabf3cbd58542f60ce030
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-05-02 19:32:53 -0500

    Added wallpapers and theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { CssBaseline, useMediaQuery } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles'
 
-function App() {
+const App = () => {
+  /* THEMING AND STYLES START */
+  const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
+  const backgroundImage = prefersLightMode ? 'green' : 'dark'
+  const useStyles = makeStyles({
+    app: {
+      height: '100%',
+      backgroundImage: `url('/assets/images/background/leaf_wallpaper_${backgroundImage}.png')`,
+      backgroundRepeat: 'repeat',
+      backgroundSize: '300px',
+    }
+  })
+  const classes = useStyles()
+  const theme = React.useMemo(() =>
+    createMuiTheme({
+      palette: {
+        type: prefersLightMode ? 'light' : 'dark',
+      },
+    }),
+  [prefersLightMode]
+  )
+  /* THEMING AND STYLES END */
+
   return (
-    <div >
-      APP
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <div className={classes.app}>
+          APP
+        </div>
+    </ThemeProvider>
   )
 }
 
