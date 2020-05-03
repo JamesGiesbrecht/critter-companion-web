efeac2776ed0e4c6129a500609ed452394f57b5d
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-05-02 20:05:19 -0500

    Importing background images through React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import backgroundDark from './assets/images/background/leaf_wallpaper_dark.png'
+import backgroundGreen from './assets/images/background/leaf_wallpaper_green.png'
 import { CssBaseline, useMediaQuery } from '@material-ui/core'
 import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles'
 
 const App = () => {
   /* THEMING AND STYLES START */
   const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
-  const backgroundImage = prefersLightMode ? 'green' : 'dark'
+  const backgroundImage = prefersLightMode ? backgroundGreen : backgroundDark
   const useStyles = makeStyles({
-    app: {
-      height: '100%',
-      backgroundImage: `url('/assets/images/background/leaf_wallpaper_${backgroundImage}.png')`,
-      backgroundRepeat: 'repeat',
-      backgroundSize: '300px',
+    '@global': {
+      body: {
+        height: '100%',
+        backgroundImage: `url('${backgroundImage}')`,
+        backgroundRepeat: 'repeat',
+        backgroundSize: '300px',
+      }
     }
   })
-  const classes = useStyles()
+  useStyles()
   const theme = React.useMemo(() =>
     createMuiTheme({
       palette: {
