110acb6f7c12a1fa0c1bf5fc9178f6bde6371a30
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-05-04 18:28:39 -0500

    Reverting to single background for performance

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React, { useState } from 'react'
 import useColorScheme from './hooks/useColorScheme'
 import Critters from './containers/Critters/Critters'
-import backgroundDark from './assets/images/background/leaf_wallpaper_dark.png'
-import backgroundGreen from './assets/images/background/leaf_wallpaper_green.png'
 import { CssBaseline } from '@material-ui/core'
-import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import Layout from './containers/Layout/Layout'
-import MaterialUISampleTable from './components/MaterialUISampleTable'
 import bugsData from './assets/data/bugs.json'
 import fishData from './assets/data/fish.json'
 import Controls from './components/Controls'
 
 const App = () => {
   /* THEMING AND STYLES START */
-  const [colorScheme, toggleColorScheme] = useColorScheme()
-  const backgroundImage = colorScheme === 'light' ? backgroundGreen : backgroundDark
-  const useStyles = makeStyles({
-    '@global': {
-      body: {
-        height: '100%',
-        backgroundImage: `url('${backgroundImage}')`,
-        backgroundRepeat: 'repeat',
-        backgroundSize: '300px',
-      }
-    }
-  })
-  useStyles()
+  const [colorScheme, toggleColorScheme] = useColorScheme()  
   const theme = React.useMemo(() =>
     createMuiTheme({
       palette: {
         type: colorScheme,
       },
     }),
-  [colorScheme]
+    [colorScheme]
   )
+
   const titleHeight = 60
   /* THEMING AND STYLES END */
 
