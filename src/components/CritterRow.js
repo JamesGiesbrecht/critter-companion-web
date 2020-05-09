19a2e447f3980a55bf0613f311a90858af5c0242
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-05-09 11:17:02 -0500

    Adding blathers logo to critters not donated

--- src/components/CritterRow.js
+++ src/components/CritterRow.js
@@ -3,15 +3,40 @@ import {
   TableRow, TableCell, Checkbox, makeStyles,
 } from '@material-ui/core'
 import Months from './Months'
+import blathersLogo from '../assets/images/blathersLogo.svg'
+import AddCircle from '@material-ui/icons/AddCircle'
+import RemoveCircle from '@material-ui/icons/RemoveCircle'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   critterImgCell: {
     padding: '0 10px',
   },
   critterImg: {
     width: '40px',
   },
-})
+  status: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  icons: {
+    display: 'block',
+  },
+  blathers: {
+    height: '1.5em',
+    filter: `${theme.palette.type === 'dark' ? 'invert(100%)' : ''} opacity(20%)`,
+    '&:hover': {
+      filter: `${theme.palette.type === 'dark' ? 'invert(100%)' : ''} opacity(100%)`,
+    },
+  },
+  donated: {
+    filter: `${theme.palette.type === 'dark' ? 'invert(100%)' : ''} opacity(100%)`,
+  },
+  name: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+}))
 
 const CritterRow = ({
   critter, obtainedCritters, setObtainedCritters, isNorthern, hours,
@@ -41,8 +66,13 @@ const CritterRow = ({
       <TableCell classes={{ root: classes.critterImgCell }}>
         <img className={classes.critterImg} src={critter.image_path} alt={critter.name} />
       </TableCell>
-      <TableCell component="th" scope="critter" padding="none">
+      <TableCell classname={classes.name}>
         {critter.name}
+        <img src={blathersLogo} className={[classes.blathers, isChecked ? classes.donated : ''].join(' ')} alt="Donated" />
+        <div className={classes.newLeavingIcons}>
+          {critter.isNew && <AddCircle color="primary" />}
+          {critter.isLeaving && <RemoveCircle color="secondary" />}
+        </div>
       </TableCell>
       <TableCell>
         <Checkbox
