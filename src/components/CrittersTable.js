8b7f8c0f219038139292528fdf081ab706b44a5a
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-05-09 18:08:48 -0500

    Enable sorting on table

--- src/components/CrittersTable.js
+++ src/components/CrittersTable.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TableContainer, Table, TableBody, makeStyles } from '@material-ui/core'
 import EnhancedTableHead from './EnhancedTableHead'
 import CritterRow from './CritterRow'
@@ -20,8 +20,36 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
+const descendingComparator = (a, b, orderBy) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1
+  }
+  return 0
+}
+
+const getComparator = (order, orderBy) => {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy)
+}
+
+const stableSort = (array, comparator) => {
+  const stabilizedThis = array.map((el, index) => [el, index])
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0])
+    if (order !== 0) return order
+    return a[1] - b[1]
+  })
+  return stabilizedThis.map((el) => el[0])
+}
+
 const CrittersTable = ({ critters, isNorthern, obtainedCritters, setObtainedCritters }) => {
   const classes = useStyles()
+  const [order, setOrder] = useState('asc')
+  const [orderBy, setOrderBy] = useState('name')
 
   const amOrPM = (hour) => {
     if (hour > 12 && hour < 24) {
@@ -52,19 +80,26 @@ const CrittersTable = ({ critters, isNorthern, obtainedCritters, setObtainedCrit
     return `${amOrPM(startTime)} - ${amOrPM(endTime)}`
   }
 
-  const rows = critters.map((critter) => {
-    const hours = getHours(critter.start_time, critter.end_time)
-    return (
-      <CritterRow
-        key={critter.name}
-        critter={critter}
-        obtainedCritters={obtainedCritters}
-        setObtainedCritters={setObtainedCritters}
-        isNorthern={isNorthern}
-        hours={hours}
-      />
-    )
-  })
+  const handleSortRequest = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc'
+    setOrder(isAsc ? 'desc' : 'asc')
+    setOrderBy(property)
+  }
+
+  const rows = stableSort(critters, getComparator(order, orderBy))
+    .map((critter) => {
+      const hours = getHours(critter.start_time, critter.end_time)
+      return (
+        <CritterRow
+          key={critter.name}
+          critter={critter}
+          obtainedCritters={obtainedCritters}
+          setObtainedCritters={setObtainedCritters}
+          isNorthern={isNorthern}
+          hours={hours}
+        />
+      )
+    })
 
   return (
     <TableContainer className={classes.tableWrapper}>
@@ -74,7 +109,11 @@ const CrittersTable = ({ critters, isNorthern, obtainedCritters, setObtainedCrit
         aria-labelledby="tableTitle"
         aria-label="enhanced table"
       >
-        <EnhancedTableHead />
+        <EnhancedTableHead
+          order={order}
+          orderBy={orderBy}
+          onSortRequest={handleSortRequest}
+        />
         <TableBody>
           {rows}
         </TableBody>
--- src/components/EnhancedTableHead.js
+++ src/components/EnhancedTableHead.js
@@ -1,27 +1,32 @@
 import React from 'react'
-import { TableHead, TableRow, TableCell, TableSortLabel } from '@material-ui/core'
+import { TableHead, TableRow, TableCell, TableSortLabel, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+})
 
 const headCells = [
-  {
-    id: 'name', numeric: false, disablePadding: true, label: 'Name',
-  },
-  {
-    id: 'price', numeric: true, disablePadding: true, label: 'Price',
-  },
-  {
-    id: 'location', numeric: true, disablePadding: true, label: 'Location',
-  },
-  {
-    id: 'hours', numeric: true, disablePadding: true, label: 'Active Hours',
-  },
-  {
-    id: 'months', numeric: true, disablePadding: true, label: 'Active Months',
-  },
+  { id: 'name', numeric: false, align: 'center', label: 'Name' },
+  { id: 'value', numeric: true, align: 'right', label: 'Price' },
+  { id: 'location', numeric: false, align: 'right', label: 'Location' },
+  { id: 'start_time', numeric: false, align: 'right', label: 'Active Hours' },
+  { id: 'months', numeric: false, align: 'right', label: 'Active Months' },
 ]
 
-const EnhancedTableHead = ({ classes, order, orderBy, onRequestSort }) => {
+const EnhancedTableHead = ({ order, orderBy, onSortRequest }) => {
+  const classes = useStyles()
   const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property)
+    onSortRequest(event, property)
   }
 
   return (
@@ -31,22 +36,24 @@ const EnhancedTableHead = ({ classes, order, orderBy, onRequestSort }) => {
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
+            align={headCell.align}
             padding="none"
             sortDirection={orderBy === headCell.id ? order : false}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
+            {headCell.id !== 'months' ? (
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id && (
+                  <span className={classes.visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </span>
+                )}
+              </TableSortLabel>
+            ) : headCell.label}
           </TableCell>
         ))}
       </TableRow>
