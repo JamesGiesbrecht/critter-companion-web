72a7baaa8e22721d73e4fa9a7cae5cf90df32aa4
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-05-24 18:49:07 -0500

    Removing extra properties from the theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,28 +1,14 @@
 import { createTheme, Theme } from '@material-ui/core/styles'
-import { red, pink, teal } from '@material-ui/core/colors'
 import { PaletteMode } from '@material-ui/core'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const defaultTheme = createTheme()
 
 // Edit this function for global theme overrides
 const getTheme = (colorScheme: PaletteMode): Theme =>
   createTheme({
-    mixins: {
-      header: {
-        height: 50,
-      },
-      drawer: {
-        width: 250,
-        hidden: { mdDown: true },
-        hiddenBreakpoint: defaultTheme.breakpoints.down('md'),
-        visibleBreakpoint: defaultTheme.breakpoints.up('md'),
-      },
-    },
     palette: {
       mode: colorScheme,
-      primary: teal,
-      secondary: pink,
-      error: red,
     },
   })
 
