9dac336154788ef5513d23c0bcc77ac964a0b88b
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-05-26 20:35:16 -0500

    Optimizing the critter filtering

--- src/components/critters/Critters.jsx
+++ src/components/critters/Critters.jsx
@@ -5,36 +5,50 @@ import bugsData from 'assets/data/bugs.json'
 import fishData from 'assets/data/fish.json'
 import seaData from 'assets/data/sea.json'
 
+const today = new Date()
+const curMonth = today.getMonth() + 1
+
+const hasPrevMonth = (months) => {
+  // January
+  if (curMonth === 1) {
+    return months.includes(12)
+  }
+  return months.includes(curMonth - 1)
+}
+
+const hasNextMonth = (months) => {
+  // December
+  if (curMonth === 12) {
+    return months.includes(1)
+  }
+  return months.includes(curMonth + 1)
+}
+
+const isAvailableNow = (months) => months.includes(curMonth)
+
 const Critters = () => {
   const [showAll, setShowAll] = useState('showAll')
   const [show, setShow] = useState(['isNew', 'isLeaving', 'isIncoming', 'isDonated'])
   const [isNorthern, setIsNorthern] = useState(true)
   const [search, setSearch] = useState('')
-  const today = new Date()
-  const curMonth = today.getMonth() + 1
 
-  const hasPrevMonth = (months) => {
-    // January
-    if (curMonth === 1) {
-      return months.includes(12)
+  const getAvailability = (months) => {
+    const availability = {}
+
+    if (isAvailableNow(months)) {
+      availability.isAvailableNow = true
+      if (!hasPrevMonth(months)) {
+        availability.isNew = true
+      }
+      if (!hasNextMonth(months)) {
+        availability.isLeaving = true
+      }
+    } else if (hasNextMonth(months)) {
+      availability.isIncoming = true
     }
-    return months.includes(curMonth - 1)
+
+    return availability
   }
-
-  const hasNextMonth = (months) => {
-    // December
-    if (curMonth === 12) {
-      return months.includes(1)
-    }
-    return months.includes(curMonth + 1)
-  }
-  const isAvailableNow = (months) => months.includes(curMonth)
-
-  const isNew = (months) => isAvailableNow(months) && !hasPrevMonth(months)
-
-  const isLeaving = (months) => isAvailableNow(months) && !hasNextMonth(months)
-
-  const isIncoming = (months) => !isAvailableNow(months) && hasNextMonth(months)
 
   const addProperties = (critters) =>
     critters.map((critter) => {
@@ -42,32 +56,23 @@ const Critters = () => {
       return {
         ...critter,
         months: critterMonths,
-        isAvailableNow: isAvailableNow(critterMonths),
-        isNew: isNew(critterMonths),
-        isLeaving: isLeaving(critterMonths),
-        isIncoming: isIncoming(critterMonths),
+        ...getAvailability(critterMonths),
         isDonated: false,
       }
     })
+
+  const bugs = addProperties(bugsData)
+  const fish = addProperties(fishData)
+  const seaCreatures = addProperties(seaData)
 
   const getTables = () => {
     if (search === '') {
       return (
         <>
+          <CritterSection allCritters={bugs} type="Bugs" showAll={showAll} show={show} />
+          <CritterSection allCritters={fish} type="Fish" showAll={showAll} show={show} />
           <CritterSection
-            allCritters={addProperties(bugsData)}
-            type="Bugs"
-            showAll={showAll}
-            show={show}
-          />
-          <CritterSection
-            allCritters={addProperties(fishData)}
-            type="Fish"
-            showAll={showAll}
-            show={show}
-          />
-          <CritterSection
-            allCritters={addProperties(seaData)}
+            allCritters={seaCreatures}
             type="Sea Creatures"
             showAll={showAll}
             show={show}
@@ -77,7 +82,7 @@ const Critters = () => {
     }
     return (
       <CritterSection
-        allCritters={addProperties(bugsData).concat(addProperties(fishData))}
+        allCritters={bugs.concat(fish, seaCreatures)}
         type="Search"
         showAll={showAll}
         show={show}
