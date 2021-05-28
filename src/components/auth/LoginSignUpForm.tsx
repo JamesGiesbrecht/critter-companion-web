30084c6bc448c61721e350624c712b577a74f273
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-05-27 20:53:42 -0500

    Setting up inputs and state for auth form

--- src/components/auth/LoginSignUpForm.tsx
+++ src/components/auth/LoginSignUpForm.tsx
@@ -4,17 +4,24 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
   TextField,
+  useTheme,
 } from '@material-ui/core'
 
 type ActiveForm = 'login' | 'sign up'
 
 const LoginSignUpForm = () => {
-  const [activeForm, setActiveForm] = useState<ActiveForm | null>(null)
+  const [activeForm, setActiveForm] = useState<ActiveForm | undefined>()
+  const isLogin = activeForm === 'login'
 
-  const handleClose = () => setActiveForm(null)
+  const handleClose = () => setActiveForm(undefined)
+
+  const handleSubmit = () => {
+    console.log('Submitting')
+  }
+
+  const toggleState = () => setActiveForm((prev) => (prev === 'sign up' ? 'login' : 'sign up'))
 
   return (
     <>
@@ -24,13 +31,9 @@ const LoginSignUpForm = () => {
       <Button variant="contained" onClick={() => setActiveForm('sign up')}>
         Sign Up
       </Button>
-      <Dialog open={Boolean(activeForm)} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+      <Dialog open={Boolean(activeForm)} onClose={handleClose} aria-labelledby={activeForm}>
+        <DialogTitle id={activeForm}>{isLogin ? 'Login' : 'Sign Up'}</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
@@ -38,12 +41,34 @@ const LoginSignUpForm = () => {
             label="Email Address"
             type="email"
             fullWidth
-            variant="standard"
+            variant="filled"
           />
+          <TextField
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="filled"
+          />
+          {!isLogin && (
+            <TextField
+              margin="dense"
+              id="confirmPassword"
+              label="Confirm Password"
+              type="password"
+              fullWidth
+              variant="filled"
+            />
+          )}
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+        <DialogActions sx={{ justifyContent: 'space-between', pl: 3, pr: 3 }}>
+          <Button onClick={toggleState} size="large">
+            {`Switch to ${isLogin ? 'Sign Up' : 'Login'}`}
+          </Button>
+          <Button onClick={handleSubmit} size="large">
+            Submit
+          </Button>
         </DialogActions>
       </Dialog>
     </>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,19 @@
 import { createTheme, Theme } from '@material-ui/core/styles'
 import { PaletteMode } from '@material-ui/core'
+import { green } from '@material-ui/core/colors'
 
 // Edit this function for global theme overrides
 const getTheme = (colorScheme: PaletteMode): Theme => {
   const defaultTheme = createTheme({ palette: { mode: colorScheme } })
+  const themeVars = (light: any, dark: any) => (colorScheme === 'light' ? light : dark)
   return createTheme({
     palette: {
       mode: colorScheme,
+      primary: {
+        light: green[400],
+        main: themeVars(green[700], green[500]),
+        dark: green[800],
+      },
     },
     components: {
       MuiButton: {
