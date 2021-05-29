2e9f19cecdb20dece32a6e4505bd46b8a588f189
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-05-28 22:04:41 -0500

    Handling form validation for sign up

--- src/components/auth/LoginSignUpForm.tsx
+++ src/components/auth/LoginSignUpForm.tsx
@@ -16,38 +16,47 @@ type FormInput = 'email' | 'password' | 'confirmPassword'
 const defaultFormState = {
   email: {
     value: '',
+    label: 'Email',
+    name: 'email',
     touched: false,
     error: '',
     validation: {
       required: true,
-      email: true,
+      isEmail: true,
     },
   },
   password: {
     value: '',
+    label: 'Password',
+    name: 'password',
     touched: false,
     error: '',
     validation: {
       required: true,
-      matches: 'confirmPassword',
     },
   },
   confirmPassword: {
     value: '',
+    label: 'Confirm Password',
+    name: 'confirmPassword',
     touched: false,
     error: '',
     validation: {
       required: true,
-      matches: 'password',
+      passwordMatch: true,
     },
   },
-  formIsValid: false,
 }
 
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 const LoginSignUpForm = () => {
-  const [formState, setFormState] = useState(defaultFormState)
+  const [formState, setFormState] = useState<any>(defaultFormState)
   const [activeForm, setActiveForm] = useState<ActiveForm | undefined>()
   const isLogin = activeForm === 'login'
+
+  const { email, password, confirmPassword } = formState
 
   const handleClose = () => setActiveForm(undefined)
 
@@ -55,39 +64,97 @@ const LoginSignUpForm = () => {
     setFormState(defaultFormState)
   }, [activeForm])
 
-  const validateForm = () => {
-    const emailRegex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const inputIsValid = (name: FormInput, value?: any) => {
+    // eslint-disable-next-line no-param-reassign
+    if (!value) value = formState[name].value
+    const { validation, label } = formState[name]
+    const { required, passwordMatch, isEmail } = validation
+    let error = ''
+    if (required && !value) {
+      error = `${label} cannot be empty`
+    } else if (isEmail && !emailRegex.test(value.toLowerCase())) {
+      error = 'Enter a valid email address'
+    } else if (passwordMatch && value !== formState.password.value) {
+      error = 'Passwords must match'
+    } else if (name === 'password' && value === formState.confirmPassword.value) {
+      setFormState((prev: any) => ({
+        ...prev,
+        confirmPassword: {
+          ...prev.confirmPassword,
+          error: '',
+        },
+      }))
+    }
+    if (error) {
+      setFormState((prev: any) => ({
+        ...prev,
+        [name]: {
+          ...prev[name],
+          touched: true,
+          error,
+        },
+      }))
+      return error
+    }
+    return true
+  }
+
+  const formIsValid = () => {
+    let firstInvalidInput = ''
+    const allInputsAreValid = !Object.keys(formState)
+      .map((name: any) => {
+        const isValid = inputIsValid(name) === true
+        if (!firstInvalidInput && !isValid) {
+          firstInvalidInput = name
+        }
+        return isValid
+      })
+      .includes(false)
+    if (firstInvalidInput) {
+      document.getElementById(firstInvalidInput)?.focus()
+    }
+    return allInputsAreValid
   }
 
   const handleInputUpdate = (name: FormInput, value: string) => {
-    setFormState((prev) => ({
+    const isValid = inputIsValid(name, value)
+    const error = isValid === true ? '' : isValid
+    setFormState((prev: any) => ({
       ...prev,
       [name]: {
         ...prev[name],
         value,
+        error,
+      },
+    }))
+  }
+
+  const handleInputBlur = (name: FormInput) => {
+    setFormState((prev: any) => ({
+      ...prev,
+      [name]: {
+        ...prev[name],
+        touched: true,
       },
     }))
   }
 
   const handleSubmit = async () => {
-    try {
-      let result
-      if (isLogin) {
-        result = await firebaseAuth.signInWithEmailAndPassword(
-          formState.email.value,
-          formState.password.value,
-        )
-      } else {
-        result = await firebaseAuth.createUserWithEmailAndPassword(
-          formState.email.value,
-          formState.password.value,
-        )
+    if (formIsValid()) {
+      try {
+        let result
+        if (isLogin) {
+          result = await firebaseAuth.signInWithEmailAndPassword(email.value, password.value)
+        } else {
+          result = await firebaseAuth.createUserWithEmailAndPassword(email.value, password.value)
+        }
+        console.log(result)
+      } catch (e) {
+        console.log(e)
+        throw e
       }
-      console.log(result)
-    } catch (e) {
-      console.log(e)
-      throw e
+    } else {
+      console.log('Form Invalid')
     }
   }
 
@@ -107,34 +174,43 @@ const LoginSignUpForm = () => {
           <TextField
             autoFocus
             margin="dense"
-            id="name"
-            label="Email Address"
+            id="email"
+            label={email.label}
             type="email"
             fullWidth
             variant="filled"
-            value={formState.email.value}
+            error={Boolean(email.error)}
+            helperText={email.error}
+            value={email.value}
             onChange={(e) => handleInputUpdate('email', e.target.value)}
+            onBlur={() => handleInputBlur('email')}
           />
           <TextField
             margin="dense"
             id="password"
-            label="Password"
+            label={password.label}
             type="password"
             fullWidth
             variant="filled"
-            value={formState.password.value}
+            error={Boolean(password.error)}
+            helperText={password.error}
+            value={password.value}
             onChange={(e) => handleInputUpdate('password', e.target.value)}
+            onBlur={() => handleInputBlur('password')}
           />
           {!isLogin && (
             <TextField
               margin="dense"
               id="confirmPassword"
-              label="Confirm Password"
+              label={confirmPassword.label}
               type="password"
               fullWidth
               variant="filled"
-              value={formState.confirmPassword.value}
+              error={Boolean(confirmPassword.error)}
+              helperText={confirmPassword.error}
+              value={confirmPassword.value}
               onChange={(e) => handleInputUpdate('confirmPassword', e.target.value)}
+              onBlur={() => handleInputBlur('confirmPassword')}
             />
           )}
         </DialogContent>
